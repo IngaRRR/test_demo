1317af3eb51d2710cbe0871d263895127100ade9
= <kerinti.saule@gmail.com>
2023-04-03 20:51:52 +0300

    Add Common class

---
+++ src/test/java/lt/inga/pom/pages/Common.java
@@ -0,0 +1,44 @@
+package lt.inga.pom.pages;
+
+import lt.inga.pom.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class Common {
+
+    public static void setUpDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.closeDriver();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator); // susirandam elementa
+    }
+
+    private static List<WebElement> getElements(By locator) {
+        return Driver.getDriver().findElements(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String sendKeys) { // nurasyti teksta
+//        WebElement element = getElement(locator);
+//        element.sendKeys(sendKeys);
+        getElement(locator).sendKeys(sendKeys);
+    }
+
+    public static void clickOnElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static String getTextFromElement(By locator) { // nuskaityti teksta
+        return getElement(locator).getText();
+    }
+}
